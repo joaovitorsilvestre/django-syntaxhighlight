b99cb42189b0f930b05bbb297124f00ae3596a9a
João Vitor <joao_vitor_silvestre@outlook.com>
2016-08-17 20:22:09 -0300

    implemented multiple program languages to render in same page

--- syntaxhighlight/templatetags/syntax_highlight.py
+++ syntaxhighlight/templatetags/syntax_highlight.py
@@ -9,13 +9,8 @@ module_dir = os.path.dirname(__file__)
 register = Library()
 
 @register.simple_tag
-def code_script(args):
-    args_list = [arg.strip() for arg in args.split(',')]
-    language = args_list[0]
-    theme = args_list[1]
-
-    language_path = os.path.join(module_dir, 'languages/sh_{}.js'.format(language))
-    language_js = open(language_path).read()
+def code_script(theme):
+    theme = theme.strip()
 
     theme_path = os.path.join(module_dir, 'themes/{}_style.css'.format(theme))
     theme = open(theme_path).read()
@@ -23,36 +18,32 @@ def code_script(args):
     sh_main = open(os.path.join(module_dir, 'sh_main.js')).read()
 
     html = """
-        <script type='text/javascript'>{0};{1};</script>
-        <style type="text/css">{2}</style>
-    """.format(sh_main, language_js, theme)
+        <script type='text/javascript'>{0};</script>
+        <style type="text/css">{1}</style>
+    """.format(sh_main, theme)
 
     return mark_safe(force_text(html))
 
-def format_lines(found):
-    codigo = found.group(1).splitlines()
-
-    if codigo[0] == '':
-        codigo.pop(0)
-
-    for i, linha in enumerate(codigo):
-        codigo[i] = "<span class='linha_span'>" + linha + "</span>"
-    codigo = ''.join(codigo)
-
-    replacer = r"""
-        <div class="bloco_codigo">
-            <pre class="sh_{0}">{1}</pre>
-        </div>
-        <script type="text/javascript">sh_highlightDocument()</script>
-    """.format(LANGUAGE, codigo)
-
-    return replacer
-
-
 @register.filter
 def code_body(value,language_code):
-    global LANGUAGE
-    LANGUAGE = language_code
+    def format_lines(found):
+        codigo = found.group(1).splitlines()
+
+        if codigo[0] == '':
+            codigo.pop(0)
+
+        for i, linha in enumerate(codigo):
+            codigo[i] = "<span class='linha_span'>" + linha + "</span>"
+        codigo = ''.join(codigo)
+
+        replacer = r"""
+            <div class="bloco_codigo">
+                <pre class="sh_{0}">{1}</pre>
+            </div>
+            <script type="text/javascript">sh_highlightDocument()</script>
+        """.format(language_code, codigo)
+
+        return replacer
 
     regex = re.compile('@@(.*?)@@', re.S)
     body = re.sub(regex, format_lines, value)
@@ -66,5 +57,11 @@ def code_body(value,language_code):
     for i, item in enumerate(body):
         body[i] = item + '\n'
     body = ''.join(body)
+
+    # loads the script for the languege
+    language_path = os.path.join(module_dir, 'languages/sh_{}.js'.format(language_code))
+    language_js = open(language_path).read()
+
+    body = '<script>'+ language_js +'</script>' + body
 
     return mark_safe(force_text(body))
